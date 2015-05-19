040d83be891b87a6d3d52224a109b44bd31758cc
glopes <glopesdev@users.noreply.github.com>
2015-05-19 17:17:42 +0100

    Updated Bitalino source to use new API version and refactored properties.

--- Bonsai.Bitalino/Bitalino.cs
+++ Bonsai.Bitalino/Bitalino.cs
@@ -12,10 +12,17 @@ namespace Bonsai.Bitalino
     {
         static readonly int[] EmptyChannels = new int[0];
 
+        public Bitalino()
+        {
+            BatteryThreshold = 10;
+            BufferLength = 100;
+            SamplingRate = 1000;
+        }
+
         [TypeConverter(typeof(AddressConverter))]
         public string Address { get; set; }
 
-        public int Battery { get; set; }
+        public int BatteryThreshold { get; set; }
 
         public int SamplingRate { get; set; }
 
@@ -37,26 +44,30 @@ namespace Bonsai.Bitalino
                     {
                         using (var bitalino = new global::Bitalino(Address))
                         {
-                            bitalino.battery(Battery);
+                            bitalino.battery(BatteryThreshold);
                             bitalino.start(SamplingRate, Channels ?? EmptyChannels, Simulated);
 
-                            var ledState = new bool[4];
                             var frames = new global::Bitalino.Frame[BufferLength];
                             for (int i = 0; i < frames.Length; i++)
                             {
                                 frames[i] = new global::Bitalino.Frame();
                             }
 
+                            var ledState = new bool[4];
+                            ledState[2] = true;
+                            bitalino.trigger(ledState);
+
                             do
                             {
-                                ledState[2] = !ledState[2];
-                                bitalino.trigger(ledState);
                                 bitalino.read(frames);
 
                                 var output = new BitalinoDataFrame(frames);
                                 observer.OnNext(output);
                             }
                             while (!cancellationToken.IsCancellationRequested);
+
+                            ledState[2] = false;
+                            bitalino.trigger(ledState);
                             bitalino.stop();
                         }
                     }
