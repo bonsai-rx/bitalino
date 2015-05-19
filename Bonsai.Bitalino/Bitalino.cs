fcf824e85d8c8eccce24484ed04ae0fa5f7ab473
glopes <glopesdev@users.noreply.github.com>
2015-05-19 02:10:31 +0100

    Added first draft of BITalino library for Bonsai.

---
+++ Bonsai.Bitalino/Bitalino.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Bitalino
+{
+    public class Bitalino : Source<BitalinoDataFrame>
+    {
+        static readonly int[] EmptyChannels = new int[0];
+
+        [TypeConverter(typeof(AddressConverter))]
+        public string Address { get; set; }
+
+        public int Battery { get; set; }
+
+        public int SamplingRate { get; set; }
+
+        public int BufferLength { get; set; }
+
+        [TypeConverter(typeof(UnidimensionalArrayConverter))]
+        [Description("The channels to include in the output buffer. Reordering and duplicating channels is allowed.")]
+        public int[] Channels { get; set; }
+
+        public bool Simulated { get; set; }
+
+        public override IObservable<BitalinoDataFrame> Generate()
+        {
+            return Observable.Create<BitalinoDataFrame>((observer, cancellationToken) =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        using (var bitalino = new global::Bitalino(Address))
+                        {
+                            bitalino.battery(Battery);
+                            bitalino.start(SamplingRate, Channels ?? EmptyChannels, Simulated);
+
+                            var ledState = new bool[4];
+                            var frames = new global::Bitalino.Frame[BufferLength];
+                            for (int i = 0; i < frames.Length; i++)
+                            {
+                                frames[i] = new global::Bitalino.Frame();
+                            }
+
+                            do
+                            {
+                                ledState[2] = !ledState[2];
+                                bitalino.trigger(ledState);
+                                bitalino.read(frames);
+
+                                var output = new BitalinoDataFrame(frames);
+                                observer.OnNext(output);
+                            }
+                            while (!cancellationToken.IsCancellationRequested);
+                            bitalino.stop();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        observer.OnError(ex);
+                    }
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+            });
+        }
+
+        class AddressConverter : StringConverter
+        {
+            public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+            {
+                return true;
+            }
+
+            public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+            {
+                var devices = global::Bitalino.find();
+                return new StandardValuesCollection(Array.ConvertAll(devices, device => device.macAddr));
+            }
+        }
+    }
+}
---
+++ Bonsai.Bitalino/BitalinoDataFrame.cs
@@ -0,0 +1,60 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Bitalino
+{
+    public class BitalinoDataFrame
+    {
+        public BitalinoDataFrame(global::Bitalino.Frame[] frames)
+        {
+            if (frames == null)
+            {
+                throw new ArgumentNullException("frames");
+            }
+
+            if (frames.Length == 0)
+            {
+                throw new ArgumentException("The array of BITalino frames cannot be empty.");
+            }
+
+            Analog = GetAnalogData(frames);
+            Digital = GetDigitalData(frames);
+        }
+
+        static Mat GetAnalogData(global::Bitalino.Frame[] frames)
+        {
+            var data = new short[frames.Length * frames[0].analog.Length];
+            for (int j = 0; j < frames.Length; j++)
+            {
+                for (int i = 0; i < frames[j].analog.Length; i++)
+                {
+                    data[i * frames.Length + j] = frames[j].analog[i];
+                }
+            }
+
+            return Mat.FromArray(data, frames[0].analog.Length, frames.Length, Depth.S16, 1);
+        }
+
+        static Mat GetDigitalData(global::Bitalino.Frame[] frames)
+        {
+            var data = new byte[frames.Length * frames[0].digital.Length];
+            for (int j = 0; j < frames.Length; j++)
+            {
+                for (int i = 0; i < frames[j].digital.Length; i++)
+                {
+                    data[i * frames.Length + j] = frames[j].digital[i] ? (byte)1 : (byte)0;
+                }
+            }
+
+            return Mat.FromArray(data, frames[0].analog.Length, frames.Length, Depth.U8, 1);
+        }
+
+        public Mat Analog { get; private set; }
+
+        public Mat Digital { get; private set; }
+    }
+}
